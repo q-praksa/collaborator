3761bf8138bd428fa6e7075a8661b7bd2eaeef5f
Zola987 <laza.ivosevic87@gmail.com>
2022-01-10 14:01:45 +0100

    [CLB-30] Added Changes

--- src/pages/Timesheet/index.tsx
+++ src/pages/Timesheet/index.tsx
@@ -1,16 +1,32 @@
 import Calendar from '@components/Timesheet';
 import styles from './Timesheet.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@reduxStore/reducers';
+import { open } from '@reduxStore/actions/modal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import AddNewTimesheet from '@components/modals/AddNewTimeSheet/AddNewTimesheet';
 
 const month = new Date().toLocaleString('default', { month: 'long' });
 const year = new Date().getFullYear();
 
 const TimesheetPg = () => {
+    const dispatch = useDispatch();
+    const modal = useSelector((state: RootState) => state.modal.show);
+
     return (
         <div className={styles.wrapper}>
             <h2 className={styles.header}>
                 {month} {year}
             </h2>
+            <button className={styles.plus}>
+                <FontAwesomeIcon
+                    icon={faPlus}
+                    onClick={() => dispatch(open())}
+                />
+            </button>
             <Calendar />
+            <div>{modal ? <AddNewTimesheet /> : null}</div>
         </div>
     );
 };
