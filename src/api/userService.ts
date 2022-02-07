d2ef92efe1c1761acba816a46d1d773764a52164
SreckoStojic <stojic.srecko@gmail.com>
2022-02-07 16:56:19 +0100

    [CLB-71] Integrate updating user. Add SaveButton element.

--- src/api/userService.ts
+++ src/api/userService.ts
@@ -1,9 +1,32 @@
 import apiInstance from '@api/api';
+import jwt from 'jwt-decode';
+import { userPayloadType, userTokenType } from './types';
 
 export async function getUsers() {
     let response;
     try {
         response = await apiInstance.get('/users');
+    } catch (error) {
+        console.log(error);
+    }
+    return response;
+}
+
+export async function getUserByToken(token: string) {
+    let response;
+    const user: userTokenType = jwt(token);
+    try {
+        response = await apiInstance.get(`/users/${user.userId}`);
+    } catch (error) {
+        console.log(error);
+    }
+    return response;
+}
+
+export async function updateUser(payload: userPayloadType) {
+    let response;
+    try {
+        response = await apiInstance.patch('/users/', payload);
     } catch (error) {
         console.log(error);
     }
