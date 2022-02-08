bd86a37122a78d1307dd22780e127c076e7b2bcc
Miljana Botunjac <miljanabotunjac@gmail.com>
2022-02-08 12:28:52 +0100

    [CLB-58] Create Sign Up Page

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,36 +1,115 @@
-import { useState } from 'react';
+import { useState, FormEvent, SetStateAction } from 'react';
 import styles from './SignUp.module.css';
 import { useTranslation } from 'react-i18next';
 import { positions } from '@constants/employees';
-import signUp from '@api/signupService';
 import TextInput from '@elements/Inputs/TextInput';
 
 export default function SignUp(): React.ReactElement {
     const { t } = useTranslation();
 
-    const [email] = useState('');
-    const [password] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleName = (e: { target: { value: SetStateAction<string> } }) => {
+        setName(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handleLastName = (e: {
+        target: { value: SetStateAction<string> };
+    }) => {
+        setLastName(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handleEmail = (e: { target: { value: SetStateAction<string> } }) => {
+        setEmail(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handlePassword = (e: {
+        target: { value: SetStateAction<string> };
+    }) => {
+        setPassword(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handleSubmit = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        if (name === '' || lastName === '' || email === '' || password === '') {
+            setError(true);
+        } else {
+            setSubmitted(true);
+            setError(false);
+        }
+    };
+
+    const successMessage = () => {
+        return (
+            <div
+                className="success"
+                style={{
+                    display: submitted ? '' : 'none',
+                }}
+            >
+                <h1>
+                    {' '}
+                    Welcome {name} {lastName}{' '}
+                </h1>
+            </div>
+        );
+    };
+
+    const errorMessage = () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: error ? '' : 'none',
+                }}
+            >
+                <h1>Please enter all the fields</h1>
+            </div>
+        );
+    };
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.form}>
                 <h1>{t(`description.signUp`)}</h1>
                 <p>{t(`description.signUpPageText`)}</p>
+                <div className="messages">
+                    {errorMessage()}
+                    {successMessage()}
+                </div>
                 <TextInput
                     label={`${t('description.firstName')}:`}
                     name="firstName"
                     type="text"
+                    onChange={handleName}
                 />
                 <TextInput
                     label={`${t('description.lastName')}:`}
                     name="lastName"
                     type="text"
+                    onChange={handleLastName}
                 />
-                <TextInput label="Email:" name="email" type="email" />
+                <TextInput
+                    label="Email:"
+                    name="email"
+                    type="email"
+                    onChange={handleEmail}
+                />
                 <TextInput
                     label={`${t('description.password')}:`}
                     name="password"
                     type="password"
+                    onChange={handlePassword}
                 />
                 <div className={styles.select_input_wrapper}>
                     <label className={styles.select_label}>
@@ -44,9 +123,9 @@ export default function SignUp(): React.ReactElement {
                         ))}
                     </select>
                     <button
-                        className={styles['submit']}
+                        className={styles.submit}
                         type="submit"
-                        onClick={() => signUp({ email, password })}
+                        onClick={handleSubmit}
                     >
                         Sign up
                     </button>
