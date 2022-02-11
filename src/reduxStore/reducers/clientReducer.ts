bdac4194b1cde45b8c848b41e9824684fef2918f
Bojana Buljic <bojana.buljic@gmail.com>
2022-02-11 16:29:55 +0100

    [CLB-73] Add clientsReducer

---
+++ src/reduxStore/reducers/clientReducer.ts
@@ -0,0 +1,28 @@
+import actionTypes from '@reduxStore/actions/clientActionTypes';
+import { removeClient } from '@utils/clients';
+
+const initalState = {
+    clients: [],
+};
+
+const clientsReducer = (state = initalState, action: any) => {
+    switch (action.type) {
+        case actionTypes.GET_CLIENTS: {
+            return {
+                ...state,
+                clients: [...action.payload],
+            };
+        }
+        case actionTypes.DELETE_CLIENT: {
+            const newClientsState = removeClient(state.clients, action.payload);
+            return {
+                ...state,
+                clients: [...newClientsState],
+            };
+        }
+        default:
+            return state;
+    }
+};
+
+export default clientsReducer;
